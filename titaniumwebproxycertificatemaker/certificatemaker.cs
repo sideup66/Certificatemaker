fbe4e3ed8783b83f65eba249c5c50dc6d515d8d7
Vincent <vincentwl2309@msn.com>
2020-04-07 21:02:13 -0400

    fleshed out the installer and added a reinstall switch

--- titaniumwebproxycertificatemaker/certificatemaker.cs
+++ titaniumwebproxycertificatemaker/certificatemaker.cs
@@ -23,7 +23,11 @@ namespace titaniumwebproxycertificatemaker
                     //Uninstall the certificate
                     Console.WriteLine("Uninstalling certificate, please wait...");
                 }
-                
+                else if (arg.ToString().ToLower().Contains("/reinstall"))
+                {
+                    //reinstall the certificate by uninstalling, then installing the certificate
+                    Console.WriteLine("Reinstalling certificate, please wait...");
+                }
                 else
                 {
                     //print a help message
@@ -42,9 +46,18 @@ namespace titaniumwebproxycertificatemaker
             Console.WriteLine("Certificatemaker instructions");
             Console.WriteLine("CertificateMaker /install to generate and install the certificate");
             Console.WriteLine("CertificateMaker /uninstall to remove the existing certificate");
+            Console.WriteLine("CertificateMaker /reinstall to remove the existing certificate and install a new one");
             Console.WriteLine("Any other switch or no switch will display this help message");
             Console.WriteLine("Press any key to continue...");
             Console.ReadKey();
+        }
+        private static void InstallCert()
+        {
+            //make a certificate and install it
+        }
+        private static void UninstallCert()
+        {
+            //uninstall the created certificate
         }
     }
 }
